71e45b90fef7093a1d6a2cba654530865aeaf57f
Christophar Antony <bosc.dev@gmail.com>
2023-05-08 00:40:46 +0530

    substract

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -15,4 +15,14 @@ void main() {
   // var num1 = int.parse(input1);
   // var num2 = int.parse(input2);
   // print('$num1 + $num2 = ${num1 + num2}');
+
+  //? 3.Substract
+  // print('Enter two numbers');
+  // var input1 = stdin.readLineSync();
+  // var input2 = stdin.readLineSync();
+  // if (input1!.isEmpty) input1 = '0';
+  // if (input2!.isEmpty) input2 = '0';
+  // int num1 = int.parse(input1);
+  // int num2 = int.parse(input2);
+  // print('$num1 - $num2 = ${num1 - num2}');
 }

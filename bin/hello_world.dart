ba937f9f6ec4f3de36b327a81d633e298fccb3da
Christophar Antony <bosc.dev@gmail.com>
2023-05-08 00:29:45 +0530

    sum_of_two_numbers

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -5,5 +5,14 @@ void main() {
   // print('Who is this ?');
   // var name = stdin.readLineSync();
   // print('Nice to meet you $name');
-  
+
+  //? 2.Sum of two numbers
+  // print('Enter two numbers');
+  // var input1 = stdin.readLineSync();
+  // var input2 = stdin.readLineSync();
+  // if (input1!.isEmpty) input1 = '0';
+  // if (input2!.isEmpty) input2 = '0';
+  // var num1 = int.parse(input1);
+  // var num2 = int.parse(input2);
+  // print('$num1 + $num2 = ${num1 + num2}');
 }
